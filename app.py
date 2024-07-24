abdfc8cb145e8d40fe23e9b4caed76adce4776f5
Santi Baez <s.baez@itecriocuarto.org.ar>
2024-07-24 20:01:30 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,164 @@
+from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/tienda_celulares_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models import Marca, Categoria, Proveedor, Stock, Accesorios, Caracteristicas, Fabricante, Modelo, Equipo 
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/list_marca", methods=['POST', 'GET'])
+def marcas():
+    marcas = Marca.query.all()
+
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        nueva_marca = Marca(
+            nombre=nombre,
+        )
+        db.session.add(nueva_marca)
+        db.session.commit()
+        return redirect(url_for('marcas'))
+
+    return render_template('list_marca.html', marcas=marcas)
+
+@app.route("/list_categorias", methods=['POST', 'GET'])
+def categorias():
+    categorias = Categoria.query.all()
+
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        nueva_categoria = Categoria(
+            nombre=nombre,
+        )
+        db.session.add(nueva_categoria)
+        db.session.commit()
+        return redirect(url_for('categorias'))
+
+    return render_template('list_categorias.html', categorias=categorias)
+
+@app.route("/list_fabricantes", methods=['POST', 'GET'])
+def fabricantes():
+    fabricantes = Fabricante.query.all()
+
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        origen = request.form['origen']
+        
+        nuevoFabricante = Fabricante(
+            nombre=nombre,
+            origen=origen
+        )
+        db.session.add(nuevoFabricante)
+        db.session.commit()
+        return redirect(url_for('fabricantes'))
+
+    return render_template('list_fabricantes.html', fabricantes=fabricantes)
+
+@app.route("/list_modelos", methods = ['POST', 'GET'])
+def modelos():
+    modelos = Modelo.query.all()
+    fabricantes = Fabricante.query.all()
+    categorias = Categoria.query.all()
+    
+    if request.method == 'POST':
+        modelo = request.form['modelo']
+        anio = request.form['anioLanzamiento']
+        sistOp = request.form['sistemaOperativo']
+        fabricante = request.form['fabricante']
+        categoria = request.form['categoria']
+        nuevoModelo = Modelo(
+            modelo=modelo,
+            anioLanzamiento=anio,
+            sistemaOperativo=sistOp,
+            fabricante_id=fabricante,
+            categoria_id=categoria
+        )
+        db.session.add(nuevoModelo)
+        db.session.commit()
+        return redirect(url_for('modelos'))
+
+    return render_template(
+        'list_modelos.html',
+        modelos=modelos,
+        fabricantes=fabricantes,
+        categorias=categorias,
+    )
+
+@app.route("/list_accesorios", methods=['POST', 'GET'])
+def accesorios():
+    accesorios = Accesorios.query.all()
+
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        descripcion = request.form['descripcion']
+        
+        nuevoAccesorio = Accesorios(
+            nombre=nombre,
+            descripcion=descripcion
+        )
+        db.session.add(nuevoAccesorio)
+        db.session.commit()
+        return redirect(url_for('accesorios'))
+
+    return render_template('list_accesorios.html', accesorios=accesorios)
+
+@app.route("/list_proveedores", methods=['POST', 'GET'])
+def proveedores():
+    proveedores = Proveedor.query.all()
+
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        contacto = request.form['contacto']
+        nuevoProveedor = Proveedor(
+            nombre=nombre,
+            contacto=contacto
+        )
+        db.session.add(nuevoProveedor)
+        db.session.commit()
+        return redirect(url_for('proveedores'))
+
+    return render_template('list_proveedores.html', proveedores=proveedores)
+
+@app.route("/list_Stock", methods=['POST', 'GET'])
+def añadirStock():
+    añadirStock = Stock.query.all()
+
+    if request.method == 'POST':
+        cantDisp = request.form['cantDisp']
+        uniAlm = request.form['ubiAlm']
+        nuevoStock = Stock(
+            cantidadDisponible=cantDisp,
+            ubicacionAlmacen=uniAlm
+        )
+        db.session.add(nuevoStock)
+        db.session.commit()
+        return redirect(url_for('añadirStock'))
+
+    return render_template('list_Stock.html', añadirStock=añadirStock)
+
+@app.route("/list_caracteristicas", methods=['POST', 'GET'])
+def añadirCaracteristica():
+    añadirCaracteristica = Caracteristicas.query.all()
+
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        descripcion = request.form['descripcion']  
+
+        nuevaCaracteristica = Caracteristicas(
+            nombre=nombre,
+            descripcion=descripcion,
+        )
+        db.session.add(nuevaCaracteristica)
+        db.session.commit()
+        return redirect(url_for('añadirCaracteristica'))  
+
+    return render_template('list_caracteristicas.html', añadirCaracteristica=añadirCaracteristica)
---
+++ models.py
@@ -0,0 +1,92 @@
+from app import db
+
+class Marca(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+
+    def __str__(self) -> str:
+        return self.nombre
+    
+class Categoria(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+
+    def __str__(self) -> str:
+        return self.nombre
+    
+class Proveedor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    contacto = db.Column(db.String(50), nullable=False)
+
+    def __str__(self) -> str:
+        return self.nombre
+    
+class Stock(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cantidadDisponible = db.Column(db.Integer, nullable=False)
+    ubicacionAlmacen = db.Column(db.String(10), nullable=False)
+
+    def __str__(self) -> str:
+        return self.cantidadDisponible
+    
+class Accesorios(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    descripcion = db.Column(db.String(1000), nullable=False)
+
+    def __str__(self) -> str:
+        return self.nombre
+    
+class Caracteristicas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    descripcion = db.Column(db.String(1000), nullable=False)  
+
+    def __str__(self) -> str:
+        return self.nombre
+
+
+    
+class Fabricante(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    origen = db.Column(db.String(50), nullable=False)
+
+    def __str__(self) -> str:
+        return self.nombre    
+    
+class Modelo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    modelo = db.Column(db.String(50), nullable=False)
+    anioLanzamiento = db.Column(db.Integer)
+    sistemaOperativo = db.Column(db.String(50), nullable=False)
+    categoria = db.Column(db.String(50), nullable=False)
+
+    fabricante_id = db.Column(db.Integer, db.ForeignKey('fabricante.id'), nullable=False)
+    fabricante = db.relationship('Fabricante', backref=db.backref('modelos', lazy=True))
+
+    categoria_id = db.Column(db.Integer, db.ForeignKey('categoria.id'), nullable=False)
+    categoria = db.relationship('Categoria', backref=db.backref('modelos', lazy=True))
+
+
+class Equipo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    categoria = db.Column(db.String(50), nullable=False)
+    precio = db.Column(db.Integer)
+
+    modelo_id = db.Column(db.Integer, db.ForeignKey('modelo.id'), nullable=False)
+    marca_id = db.Column(db.Integer, db.ForeignKey('marca.id'), nullable=False)  
+    caracteristicas_id = db.Column(db.Integer, db.ForeignKey('caracteristicas.id'), nullable=False)
+    proveedor_id = db.Column(db.Integer, db.ForeignKey('proveedor.id'), nullable=False)
+    stock_id = db.Column(db.Integer, db.ForeignKey('stock.id'), nullable=False)
+    accesorios_id = db.Column(db.Integer, db.ForeignKey('accesorios.id'), nullable=False)
+
+    modelo = db.relationship('Modelo', backref=db.backref('equipos', lazy=True))
+    marca = db.relationship('Marca', backref=db.backref('equipos', lazy=True))
+    caracteristicas = db.relationship('Caracteristicas', backref=db.backref('equipos', lazy=True))
+    proveedor = db.relationship('Proveedor', backref=db.backref('equipos', lazy=True))
+    stock = db.relationship('Stock', backref=db.backref('equipos', lazy=True))
+    accesorios = db.relationship('Accesorios', backref=db.backref('equipos', lazy=True))
+
